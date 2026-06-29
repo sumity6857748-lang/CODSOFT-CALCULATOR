b5a3c132b96253386952749ff861b1fce10f879e
sumity6857748-lang <sumity6857748@gmail.com>
2026-06-29 11:55:04 +0530

    Add basic calculator functionality in script.js
    add script .js

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById("display");
+
+function append(value){
+display.value += value;
+}
+
+function clearDisplay(){
+display.value = "";
+}
+
+function calculate(){
+try{
+display.value = eval(display.value);
+}
+catch{
+display.value = "Error";
+}
+}
